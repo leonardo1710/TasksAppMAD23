c94e26f651882d040c2508aa5965dcbf58d12324
leonardo1710 <leon.freudenthaler@fh-campuswien.ac.at>
2023-04-04 22:44:51 +0200

    Dependencies and Entity

--- app/src/main/java/com/example/tasksapplication/models/Task.kt
+++ app/src/main/java/com/example/tasksapplication/models/Task.kt
@@ -3,18 +3,26 @@ package com.example.tasksapplication.models
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 
+@Entity
 data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val label: String,
-    val initialIsDone: Boolean = false
+    @Ignore
+    val initialIsDone: Boolean = false,
+    val isDone: Boolean
 ) {
-    var isDone by mutableStateOf(initialIsDone)
+    //var isDone by mutableStateOf(initialIsDone)
 }
 
 fun getTasks(): List<Task> {
     return listOf(
-        Task("read a book", false),
-        Task( "learn mad", false),
-        Task( "water the flowers", true)
+        Task(label = "read a book", isDone = false),
+        Task( label = "learn mad", isDone = false),
+        Task( label = "water the flowers", isDone = true)
     )
 }
